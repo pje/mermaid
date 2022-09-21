8d052a22acf1e94554e55a1e68e276acf883e22c
Patrick Ellis <patrick.j.ellis@gmail.com>
2022-09-21 18:18:01 -0400

    Update sequenceDiagram grammar: allow `-` and `:` in participant names

--- src/diagrams/sequence/parser/sequenceDiagram.spec.js
+++ src/diagrams/sequence/parser/sequenceDiagram.spec.js
@@ -13,9 +13,13 @@ describe('parsing a sequence diagram', function () {
   });
 
   it('should parse a basic valid Sequence Diagram', function () {
-    const res = sequence.parser.parse(
-      'sequenceDiagram; participant A; participant B; A->>B: Hi; B->>A: Hello'
-    );
+    const res = sequence.parser.parse(`
+      sequenceDiagram
+        participant A
+        participant B
+        A->>B: Hi
+        B->>A: Hello
+    `);
 
     const actors = sequence.parser.yy.getActors();
     const actorA = actors['A'];
@@ -34,6 +38,38 @@ describe('parsing a sequence diagram', function () {
       name: 'B',
       description: 'B',
       prevActor: 'A',
+    };
+
+    expect(actorA).toEqual(expect.objectContaining(expectedA));
+    expect(actorB).toEqual(expect.objectContaining(expectedB));
+  });
+
+  it('should parse actor names that have colons and hyphens', function () {
+    const res = sequence.parser.parse(`
+      sequenceDiagram
+        participant A[fa:fa-foo] as A
+        participant B[fa:fa-bar] as B
+        A->>B: Hi
+        B->>A: Hello
+    `);
+
+    const actors = sequence.parser.yy.getActors();
+    const actorA = actors['A[fa:fa-foo]'];
+    const actorB = actors['B[fa:fa-bar]'];
+
+    expect(Object.keys(actors).sort()).toEqual(['A', 'A[fa:fa-foo]', 'B', 'B[fa:fa-bar]'].sort());
+
+    const expectedA = {
+      type: 'participant',
+      name: 'A[fa:fa-foo]',
+      description: 'A',
+      prevActor: undefined,
+    };
+    const expectedB = {
+      type: 'participant',
+      name: 'B[fa:fa-bar]',
+      description: 'B',
+      prevActor: 'A[fa:fa-foo]',
     };
 
     expect(actorA).toEqual(expect.objectContaining(expectedA));
